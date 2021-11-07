39be09a622e253873b2954db0e60a5fc39964401
Prabin Magar <psmagar123451@gmail.com>
2021-11-07 18:40:22 +0545

    Source Code

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// navbar toggling
+const navbarShowBtn = document.querySelector('.navbar-show-btn');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+const navbarHideBtn = document.querySelector('.navbar-hide-btn');
+
+navbarShowBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.add('navbar-show');
+});
+navbarHideBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.remove('navbar-show');
+});
+
+// changing search icon image on window resize
+window.addEventListener('resize', changeSearchIcon);
+function changeSearchIcon(){
+    let winSize = window.matchMedia("(min-width: 1200px)");
+    if(winSize.matches){
+        document.querySelector('.search-icon img').src = "images/search-icon.png";
+    } else {
+        document.querySelector('.search-icon img').src = "images/search-icon-dark.png";
+    }
+}
+changeSearchIcon();
+
+// stopping all animation and transition
+let resizeTimer;
+window.addEventListener('resize', () =>{
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
